525568df4d6032c393b7f539ca9c94f4ab75a77d
hanunsusanti <21081010296@student.upnjatim.ac.id>
2024-07-14 09:56:37 +0700

    Update

--- js/navbar.js
+++ js/navbar.js
@@ -1,28 +1,6 @@
-$(document).ready(function() {
-    $(".nav-menu").hover(function(event) {
-        // Buat elemen tooltip
-        var tooltip = $("<div class='tooltip'></div>");
-        
-        // Ambil keterangan dari atribut data-tooltip
-        var tooltipText = $(this).attr("data-tooltip");
-        tooltip.text(tooltipText);
-        
-        // Tambahkan tooltip ke body
-        $("body").append(tooltip);
-        
-        // Posisi tooltip di sebelah kanan elemen yang sedang dihover
-        var left = $(this).offset().left + $(this).width() + 5; // 5px jarak dari ikon
-        var top = $(this).offset().top + $(this).height() / 2 - tooltip.height() / 2;
-        tooltip.css({ left: left, top: top });
-        
-        // Tampilkan tooltip
-        tooltip.fadeIn();
-    }, function() {
-        // Hapus tooltip saat mouse keluar
-        $(".tooltip").remove();
-    });
-    $(".nav-menu").click(function() {
-        // Hapus tooltip saat ikon diklik
-        $(".tooltip").remove();
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
     });
 });
